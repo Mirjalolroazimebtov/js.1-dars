924d4e95f18612e4d07e8506d8edca10bcaacdec
Husan <shomurodovhusan130@gmail.com>
2024-09-10 19:38:30 +0500

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+var name = prompt ("Ismingizni kiriting")
+console.log(`Sizning ismingiz: ${name}`);
+
+var age = +prompt ("Yoshingizni kiriting")
+console.log(`Sizning Yoshingiz: ${age}`);
+
+var misol = +prompt(`5+3=?`)
+console.log(`5+3=8 sizning javobingiz: ${misol}`)
+
+var misol = +prompt(`5-3=?`)
+console.log(`5-3=2 sizning javobingiz: ${misol}`)
+
+var misol = +prompt(`5*3=?`)
+console.log(`5*3=15 sizning javobingiz: ${misol}`)
+
+var misol = +prompt(`9/3=?`)
+console.log(`9/3=3 sizning javobingiz: ${misol}`)
+
+var misol = +prompt(`10/3=?`)
+console.log(`10/3=1 sizning javobingiz: ${misol}`)
